5d6075cd06cb3bb962fd02ab132d78de11cbe504
Eddie Krill <scherenhaenden@gmail.com>
2020-10-14 21:24:02 +0200

    Draft modules for angular

--- TourManagerUI/src/app/app-routing.module.ts
+++ TourManagerUI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShowVenuesComponent } from './views/venues/show-venues/show-venues.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: ShowVenuesComponent  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ TourManagerUI/src/app/views/venues/venues.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShowVenuesComponent } from './show-venues/show-venues.component';
+
+
+
+@NgModule({
+  declarations: [ShowVenuesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class VenuesModule { }

60f4db26960fad22ce6f51e334e670bdda99a875
Eddie Krill <scherenhaenden@gmail.com>
2020-10-14 15:41:24 +0200

    Darft spa start up

--- TourManagerBackEnd/TourManagerBackEnd/Startup.cs
+++ TourManagerBackEnd/TourManagerBackEnd/Startup.cs
@@ -11,6 +11,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+//using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+//using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
 
 namespace TourManagerBackEnd
 {
@@ -27,6 +39,10 @@ namespace TourManagerBackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "wwwroot";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +60,9 @@ namespace TourManagerBackEnd
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa => { spa.Options.SourcePath = "wwwroot"; });
             
             Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
         }

906d1c7bd25830020eb1d19c5cd57d98b7aa4e20
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 13:00:12 +0200

    Create venues model

---
+++ TourManagerUI/src/app/models/venues/venues-model.ts
@@ -0,0 +1,9 @@
+export class VenuewsModels {
+    public id = 0;
+    public name = '';
+    public address = '';
+    public number = '';
+    public contactPersons = '';
+    public loadIn = '';
+    public curfView = '';
+}

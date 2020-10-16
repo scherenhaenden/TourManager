560c66daeac8cd768d91207484746cf1dfe6f624
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 15:35:34 +0200

    Add rest model

--- TourManagerBackEnd/TourManagerBackEnd/ApiModels/VenueModel.cs
+++ TourManagerBackEnd/TourManagerBackEnd/ApiModels/VenueModel.cs
@@ -1,4 +1,13 @@
-$HEADER$namespace $NAMESPACE$
+namespace TourManagerBackEnd.ApiModels
 {
-  public class $CLASS$ {$END$}
+    public class VenueModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public string Address { get; set; } 
+        public string Number { get; set; } 
+        public string ContactPersons { get; set; } 
+        public string LoadIn { get; set; } 
+        public string CurfView { get; set; } 
+    }
 }

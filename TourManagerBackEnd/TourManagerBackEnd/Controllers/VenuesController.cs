b992b3263667dabe37d6931b7b936e2e07ed4ea3
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 14:33:54 +0200

    Add venues controller

--- TourManagerBackEnd/TourManagerBackEnd/Controllers/VenuesController.cs
+++ TourManagerBackEnd/TourManagerBackEnd/Controllers/VenuesController.cs
@@ -1,7 +1,29 @@
+using Data.Core.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace TourManagerBackEnd.Controllers
 {
-    public class VenuesController
+    [ApiController]
+    [Route("apipublic/[controller]")]
+    public class VenuesController : ControllerBase
     {
-        
+        private readonly TourManagerContext _tourManagerContext;
+
+        public VenuesController(
+            TourManagerContext tourManagerContext
+        )
+        {
+            _tourManagerContext = tourManagerContext;            
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("addvenue")]
+        public void AddVenue(dynamic values)
+        {
+            
+        }
     }
 }

944402fc16212966f07f6706e75a0f6c6ad5b09b
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 13:02:00 +0200

    Add needed modules and config

--- TourManagerUI/src/app/app.module.ts
+++ TourManagerUI/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VenuesModule } from './views/venues/venues.module';
@@ -21,6 +21,7 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }

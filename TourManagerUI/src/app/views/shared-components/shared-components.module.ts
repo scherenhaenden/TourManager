306de65ff52250ee5195ec4f471b9be052c4fd15
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 13:00:54 +0200

    Updates:
    -Add property component and forms modules
    -export the new component and change schemas to custom

--- TourManagerUI/src/app/views/shared-components/shared-components.module.ts
+++ TourManagerUI/src/app/views/shared-components/shared-components.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { PropertyInputFormularComponent } from './formular-components/property-input-formular/property-input-formular.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [MainNavComponent],
+  declarations: [MainNavComponent, PropertyInputFormularComponent],
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports:[
-    MainNavComponent    
-  ]
+    MainNavComponent, PropertyInputFormularComponent 
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class SharedComponentsModule { }

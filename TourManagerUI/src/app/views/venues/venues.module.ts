c7a62aa16e90779f00e4636ce3e60892038a4bf4
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 13:01:42 +0200

    Add needed modules to module

--- TourManagerUI/src/app/views/venues/venues.module.ts
+++ TourManagerUI/src/app/views/venues/venues.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowVenuesComponent } from './show-venues/show-venues.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { VenueInformationComponent } from './venue-information/venue-information.component';
+import { SharedComponentsModule } from '../shared-components/shared-components.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [ShowVenuesComponent],
+  declarations: [ShowVenuesComponent, VenueInformationComponent],
   imports: [
     CommonModule,
-    NgbModule
+    NgbModule,
+    SharedComponentsModule,
+    FormsModule
   ]
 })
 export class VenuesModule { }

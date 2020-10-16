024dad367c625b16e3bfd3777345928aa7da5a9e
Eddie Krill <scherenhaenden@gmail.com>
2020-10-16 12:58:45 +0200

    Add component to show venues

---
+++ TourManagerUI/src/app/views/venues/venue-information/venue-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-venue-information',
+  templateUrl: './venue-information.component.html',
+  styleUrls: ['./venue-information.component.less']
+})
+export class VenueInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

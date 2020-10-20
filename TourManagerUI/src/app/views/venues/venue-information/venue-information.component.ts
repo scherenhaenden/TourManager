b33e2e237276fc0103e6a240873fb1a7ed8fe1c2
Eddie Krill <scherenhaenden@gmail.com>
2020-10-20 10:07:13 +0200

    Draft view of venues

--- TourManagerUI/src/app/views/venues/venue-information/venue-information.component.ts
+++ TourManagerUI/src/app/views/venues/venue-information/venue-information.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VenuesModels } from 'src/app/models/venues/venues-model';
+import { VenuesService } from 'src/app/services/venues-service';
 
 @Component({
   selector: 'app-venue-information',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VenueInformationComponent implements OnInit {
 
-  constructor() { }
+  public venuewsModels: VenuesModels = new VenuesModels();
+
+  public listFlatModels: FlatModel[] = [];
+
+  constructor(private venuesService: VenuesService) {
+    this.loadVenue();
+  }
 
   ngOnInit(): void {
   }
 
+  public loadVenue(): void {
+
+    const keys = Object.keys(this.venuewsModels);
+    const keysWithoutId =  keys.filter(key => key !== 'id');
+
+    for (const i in keysWithoutId) {
+
+      if (keysWithoutId.hasOwnProperty(i)) {
+        // code here
+        const flat = new FlatModel();
+        let name = keysWithoutId[i];
+
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+        flat.propertyNameOfObject = name;
+        flat.propertyValueOfObject = this.venuewsModels[i];
+        this.listFlatModels.push(flat);
+      }
+    }
+  }
+
+  public updateValue(value: string, propertyToUpdate: string): void {
+
+    this.venuewsModels[propertyToUpdate.toLocaleLowerCase()] = value;
+  }
+
+  public async saveInsertedInfo(): Promise<void>{    
+    var resilt = this.venuesService.addNewVenue(this.venuewsModels);
+  }
+}
+
+export class FlatModel{
+  public propertyNameOfObject: string;
+  public propertyValueOfObject: string;
 }

2d69a4596d9189213967af97059b3fc741d32686
Eddie Krill <scherenhaenden@gmail.com>
2020-10-22 10:05:42 +0200

    Changes:
    -Add contacts module, routing and component

--- TourManagerUI/src/app/app-routing.module.ts
+++ TourManagerUI/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactsComponent } from './views/contacts/contacts/contacts.component';
 import { ShowVenuesComponent } from './views/venues/show-venues/show-venues.component';
 import { VenueInformationComponent } from './views/venues/venue-information/venue-information.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'venues', component: ShowVenuesComponent  },  
   { path: 'venues/summary', component: VenueInformationComponent  },
   { path: 'venues/summary/:id', component: VenueInformationComponent  },
+  { path: 'contacts', component: ContactsComponent  },
 ];
 
 @NgModule({

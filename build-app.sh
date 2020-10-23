588e93ee4e389a4d9cfd8010d3395a0f266c404a
Eddie Krill <scherenhaenden@gmail.com>
2020-10-23 14:58:46 +0200

    Add enviroment

--- build-app.sh
+++ build-app.sh
@@ -1,6 +1,6 @@
 #Build Angular app
 cd ./TourManagerUI/
-ng build 
+ng build --configuration="production"
 cd ..
 
 cp ./TourManagerUI/dist/TourManagerUI/* ./TourManagerBackEnd/TourManagerBackEnd/wwwroot/ 

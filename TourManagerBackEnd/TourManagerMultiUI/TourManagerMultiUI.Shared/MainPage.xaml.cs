db8ae7948bb85ca297e94556140a776e5cf1a8a3
Edward Flores <scherenhaenden@gmail.com>
2020-10-25 22:00:53 +0100

    Test wpf

--- TourManagerBackEnd/TourManagerMultiUI/TourManagerMultiUI.Shared/MainPage.xaml.cs
+++ TourManagerBackEnd/TourManagerMultiUI/TourManagerMultiUI.Shared/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TourManagerMultiUI.Shared.ViewModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,7 @@ namespace TourManagerMultiUI
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = new ViewModelBase();
         }
     }
 }

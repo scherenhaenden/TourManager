84fe6f29fa3f79faf0e16fbdc38cfc7e7ca5e5e4
Eddie Krill <scherenhaenden@gmail.com>
2020-10-26 15:07:50 +0100

    try again

--- build-app.sh
+++ build-app.sh
@@ -4,7 +4,13 @@ npm install
 npm install -g @angular/cli > /dev/null
 ng build --configuration="production"
 cd ..
+DIR=./TourManagerBackEnd/TourManagerWeb/wwwroot/ 
 
+if [[ ! -e $DIR ]]; then
+    mkdir $DIR
+elif [[ ! -d $DIR ]]; then
+    echo "$DIR already exists but is not a directory" 1>&2
+fi
 cp ./TourManagerUI/dist/TourManagerUI/* ./TourManagerBackEnd/TourManagerWeb/wwwroot/ 
 cd ./TourManagerBackEnd/TourManagerWeb/
 dotnet tool install ElectronNET.CLI -g

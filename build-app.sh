0e4e6d9699d85ae1a3845a9b70602399db8b074c
Eddie Krill <scherenhaenden@gmail.com>
2020-10-26 14:53:25 +0100

    try to build output

--- build-app.sh
+++ build-app.sh
@@ -5,6 +5,13 @@ npm install -g @angular/cli > /dev/null
 ng build --configuration="production"
 cd ..
 
-cp ./TourManagerUI/dist/TourManagerUI/* ./TourManagerBackEnd/TourManagerBackEnd/wwwroot/ 
+cp ./TourManagerUI/dist/TourManagerUI/* ./TourManagerBackEnd/TourManagerWeb/wwwroot/ 
+cd /TourManagerBackEnd/TourManagerWeb/
+electronize build /target linux
+
+git log --tags --simplify-by-decoration --pretty="format:%ci %d"
+
+
+
 
 

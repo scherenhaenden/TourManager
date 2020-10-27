292dca43832f94e394086f2b52ace4b2162fe4a0
Eddie Krill <scherenhaenden@gmail.com>
2020-10-27 14:47:37 +0100

    Update builder

--- build-app.sh
+++ build-app.sh
@@ -1,24 +1,32 @@
 #Build Angular app
-mkdir ./TourManagerBackEnd/TourManagerWeb/wwwroot/ 
+echo 'build starting' 
+echo 'linux build' 
 
+echo 'build angular app'
 cd ./TourManagerUI/
 npm install
-npm install -g @angular/cli > /dev/null
+npm install -g @angular/cli
+
 ng build --configuration="production"
 cd ..
-DIR=./TourManagerBackEnd/TourManagerWeb/wwwroot/ 
 
-if [[ ! -e $DIR ]]; then
-    mkdir $DIR
-elif [[ ! -d $DIR ]]; then
-    echo "$DIR already exists but is not a directory" 1>&2
+DIR="./TourManagerBackEnd/TourManagerWeb/wwwroot/"
+
+if [ ! -d $DIR ]; then
+  mkdir -p $DIR;
 fi
+
 cp ./TourManagerUI/dist/TourManagerUI/* ./TourManagerBackEnd/TourManagerWeb/wwwroot/ 
 cd ./TourManagerBackEnd/TourManagerWeb/
 dotnet tool install ElectronNET.CLI -g
 electronize build /target linux
 
-git log --tags --simplify-by-decoration --pretty="format:%ci %d"
+#git log --tags --simplify-by-decoration --pretty="format:%ci %d"
+
+
+
+
+
 
 
 
